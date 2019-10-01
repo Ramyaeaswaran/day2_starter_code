e72ec0a82545564a73a6a1c0b003922edd2e7293
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-01 09:51:41 +0530

    Day2 exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Container container1 =Container(
+  //  width: 100,
+    height:100,
+
+    color: Colors.orange,
+    //margin: EdgeInsets.symmetric(vertical: 20.0),
+  );
+  Container container2 =Container(
+  //  width: 100,
+    height:100,
+    color: Colors.white,
+      child :Image(
+        image: NetworkImage('https://raw.githubusercontent.com/McLarenCollege/Flutter-Course-Notes/master/ensign.png'),
+  ),
+      );
+
+      Container container3 =Container(
+  //  width: 100,
+    height:100,
+          color: Colors.green );
+      List<Widget> containers = [container1,container2,container3];
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children : containers
+          ),
+        ),
       ),
     ),
   );
